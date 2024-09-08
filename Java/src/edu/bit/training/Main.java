44632092c9bc24fbf9ab055e3032313ba25d7f09
Sairam Sekhar Poluru <poluru.sairam@gmail.com>
2024-09-08 21:45:27 +0530

    Custom Runner
    Executing another java program in a java code

--- Java/src/edu/bit/training/Main.java
+++ Java/src/edu/bit/training/Main.java
@@ -3,8 +3,9 @@ package edu.bit.training;
 public class Main {
 	public static void main(String[] args) {
 		Actress actress1 = new Actress("Samantha", 21, 80);
-		Actress actress2 = new Actress("Trisha", 21, 80);
-		
+		Actress actress2 = new Actress("Samantha", 21, 80);
+		Actress actress3 = new Actress("Trisha", 21, 80);	
 		System.out.println(actress1.equals(actress2));//returns true
+		System.out.println(actress1.equals(actress3));//returns false
 	}
 }
--- Java/src/edu/bit/training/MyRunner.java
+++ Java/src/edu/bit/training/MyRunner.java
@@ -4,27 +4,27 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 public class MyRunner {
-
-	public static void main(String[] args) {
-		Class<?> myClass;
-		try {
-			myClass = Class.forName("Main");
-			try {
-				Method method = myClass.getMethod("main", String[].class);
-				try {
-					method.invoke(null, (Object)new String[] {});
-				} catch (IllegalAccessException e) {
-					e.printStackTrace();
-				} catch (InvocationTargetException e) {
-					e.printStackTrace();
-				}
-			} catch (NoSuchMethodException e) {
-				e.printStackTrace();
-			} catch (SecurityException e) {
-				e.printStackTrace();
-			}			
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-	}
+    public static void main(String[] args) {
+        Class<?> myClass;
+        try {
+            myClass = Class.forName("edu.bit.training.Main");
+            try {
+                Method method = myClass.getMethod("main", String[].class);
+                try {
+                    method.invoke(null, (Object) new String[] {});
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                } catch (InvocationTargetException e) {
+                    e.printStackTrace();
+                }
+            } catch (NoSuchMethodException e) {
+                e.printStackTrace();
+            } catch (SecurityException e) {
+                e.printStackTrace();
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("Class not found: " + e.getMessage());
+        }
+    }
 }
