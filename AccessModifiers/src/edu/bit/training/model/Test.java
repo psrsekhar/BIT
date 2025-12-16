fd9b82574dae779662677ffde399431225bed560
Sairam Sekhar Poluru <poluru.sairam@gmail.com>
2025-12-16 22:57:33 +0530

    Product Training

--- AccessModifiers/src/edu/bit/training/model/Test.java
+++
@@ -1,9 +0,0 @@
-package edu.bit.training.model;
-
-public class Test extends Car {
-	public static void main(String[] args) {
-		Car car = new Car();
-		car.name = "Mustang";
-		System.out.println(car.name);
-	}
-}
